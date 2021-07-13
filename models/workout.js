f13b41aaadb86cc1416b5bcb8a5f934cf3b7fd94
Heather Jackson <hmjackson917@gmail.com>
2021-07-13 17:23:17 -0400

    fixed issue of total duration not populating on map

--- models/workout.js
+++ models/workout.js
@@ -2,47 +2,55 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const workoutSchema = new Schema({
-  day: {
-    type: Date,
-    default: Date.now,
-  },
-  exercises: [
-    {
-      type: {
-        type: String,
-        trim: true,
-      },
-      name: {
-        type: String,
-        trim: true,
-      },
-      duration: {
-        type: Number,
-        default: 0,
-      },
-      weight: {
-        type: Number,
-        default: 0,
-      },
-      reps: {
-        type: Number,
-        default: 0,
-      },
-      sets: {
-        type: Number,
-        default: 0,
-      },
-      distance: {
-        type: Number,
-        default: 0,
-      },
+const workoutSchema = new Schema(
+  {
+    day: {
+      type: Date,
+      default: Date.now,
     },
-  ],
-  totalDuration: {
-    type: Number,
-    default: 0,
+    exercises: [
+      {
+        type: {
+          type: String,
+          trim: true,
+        },
+        name: {
+          type: String,
+          trim: true,
+        },
+        duration: {
+          type: Number,
+          default: 0,
+        },
+        weight: {
+          type: Number,
+          default: 0,
+        },
+        reps: {
+          type: Number,
+          default: 0,
+        },
+        sets: {
+          type: Number,
+          default: 0,
+        },
+        distance: {
+          type: Number,
+          default: 0,
+        },
+      },
+    ],
   },
+  {
+    toObject: { virtuals: true },
+    toJSON: { virtuals: true },
+  }
+);
+
+workoutSchema.virtual("totalDuration").get(function () {
+  return this.exercises.reduce((total, exercise) => {
+    return total + exercise.duration;
+  }, 0);
 });
 
 const Workout = mongoose.model("Workout", workoutSchema);
