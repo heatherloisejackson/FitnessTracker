2794c913c08f4355de3167760fa2afe85b265776
Heather Jackson <hmjackson917@gmail.com>
2021-07-13 16:05:58 -0400

    routes files with all CRUD requests and HTML connections

---
+++ routes/api.js
@@ -0,0 +1,55 @@
+const router = require("express").Router();
+const db = require("../models");
+
+router.get("/api/workouts", (req, res) => {
+  db.Workout.aggregate([{
+    $addFields: {
+      totalDuration: { $sum: "$exercises.duration" }
+    }
+  }])
+  .sort({ day: 1 })
+  .then(dbWorkouts => {
+    res.json(dbWorkouts);
+  })
+  .catch(err => {
+    res.status(400).json(err);
+  });
+});
+
+// Update current workout
+router.put("/api/workouts/:id", (req, res) => {
+  db.Workout.findOneAndUpdate(
+    { _id:req.params.id }, 
+    { $push: { exercises:req.body } }
+  )
+  .then(dbWorkout => {
+    res.json(dbWorkout);
+  })
+  .catch(err => {
+    res.status(400).json(err);
+  });
+});
+
+// Create a new workout
+router.post("/api/workouts", ({ body }, res) => {
+  db.Workout.create(body)
+  .then(dbWorkout => {
+    res.json(dbWorkout);
+  })  
+  .catch(err => {
+    res.status(400).json(err);
+  });  
+});  
+
+// Get workouts within a range
+router.get("/api/workouts/range", (req, res) => {
+  db.Workout.find({})
+  .then(dbWorkouts => {
+    res.json(dbWorkouts);
+  })    
+  .catch(err => {
+    res.status(400).json(err);
+  });    
+});    
+
+module.exports = router;
